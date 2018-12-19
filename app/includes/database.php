30ed7fe8a8fbad307e5d4b674ca613da49c093b7
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2018-12-19 14:56:38 +0100

    Added database connection file

---
+++ app/includes/database.php
@@ -0,0 +1,38 @@
+<?php
+
+// Credentials
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'banzai';
+
+// Create connection
+
+$db = mysqli_connect($host, $user, $password, $database)
+or die("Error: ". mysqli_connect_error());
+
+// Create query for db & fetch result
+
+// Fetching pending requests, because the admin has to see the pending requests.
+
+$queryAll = "SELECT * FROM pending_requests"; // fix this later, code still works
+
+$result = mysqli_query($db, $queryAll);
+
+// Create array & store from the database
+
+$pending_requests = [];
+
+while($row = mysqli_fetch_assoc($result)) {
+    $pending_requests[] = $row;
+}
+
+print_r($pending_requests);
+
+// Close connection
+
+mysqli_close($db);
+
+
+?>
