b77176ce9244376c462410b715cbe41894afbab5
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2019-01-01 19:35:41 +0100

    Now request is inserted with first_name and last_name

--- app/users/employee/requests_succes.php
+++ app/users/employee/requests_succes.php
@@ -1,7 +1,33 @@
 <?php
+    require_once '../../includes/database.php';
+
+    // Start session because I need the first and Last name of employee.
+    session_start();
+
+    // The first and lastname of employee
+    // By starting session, it knows who you are and for example also knows that your last_name is Phan.
+    
+    $first_name = $_SESSION['name'];
+    $last_name = $_SESSION['last_name'];
+
+
+    $db = mysqli_connect($host, $user, $password, $database)
+    or die("Error: ". mysqli_connect_error());
+
     foreach ($_POST['request'] as $key => $value) {
-        echo $value . "<br />";
+      $sql = "INSERT INTO pending_requests (first_name, last_name, request)  /* Now all you have to add is the date and name and lastname */
+      VALUES ('$first_name', '$last_name','$value')";
+
     }
+
+    if(mysqli_query($db, $sql)){
+        echo "Requests inserted succesfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+    }
+
+
+
  ?>
 
  <!DOCTYPE html>
