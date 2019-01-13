66069ab3c8d6e85de09c6e4e266cd7231640c594
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2019-01-13 15:38:14 +0100

    Added new home page for employee

--- app/users/employee/home.php
+++ app/users/employee/home.php
@@ -16,14 +16,30 @@ $name = $_SESSION['name'];
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title> Hele belangrijke info MEDEWERKER!</title>
-</head>
-<body>
-    <h1> Heel belangrijk! </h1>
-    <p> U bent ingelogd als <?= $name ?> </p>
-    <a href="./requests.php"> Aanvragen </a>
-    <a href="../../logout.php"> Uitloggen </a>
-</body>
+
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="../../../app/assets/styles/css/main.css">
+    </head>
+
+    <body>
+
+      <div class="home__container">
+
+        <div class="home__left-square">
+            <div class="home__logo-wrapper">
+                <div class="home__logo">
+                    <img class="home__logo-img" src="../app/assets/images/sumo.png" />
+                </div>
+            </div>
+        </div>
+
+          <div class="home__right-square">
+
+          </div>
+
+      </div>
+
+    </body>
+
 </html>
