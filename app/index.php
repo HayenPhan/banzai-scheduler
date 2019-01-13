7f10dc1366456e2d235f385612aba9d9366bbc80
Hayen Phan <hayen@MacBook-Pro-van-Hayen.local>
2019-01-13 14:46:49 +0100

    Added error message

--- app/index.php
+++ app/index.php
@@ -13,7 +13,11 @@ or die("Error: ". mysqli_connect_error());
 
     // Check if post isset
 
-    if (isset($_POST['submit'])) {
+        // Always start with an empty error first
+        $error = "";
+
+        if (isset($_POST['submit'])) {
+
         $username = $_POST['username'];
         $password = $_POST['password'];
 
@@ -27,18 +31,16 @@ or die("Error: ". mysqli_connect_error());
         // fetch the results
         $row = mysqli_fetch_assoc($result);
 
-
         // U can do stricter checks, but for now we're just doing one simple error.
 
-        if($username == "" || $password == "") {
+        if(empty($username) || empty($password )) {
             $error = "Vul beide gegevens in";
-            print_r($error);
-
-        // this is hardcoded, try to put database info in here next time.
-        } else if ($username != $row['login_name'] || $password != $row['password_hash']) {
-            $error = "Combinatie gebruikersnaam/wachtwoord onjuist";
-            print_r($error);
         }
+
+        if ($username != $row['login_name'] || $password != $row['password_hash']) {
+            $error = "Combinatie gebruikersnaam/wachtwoord onjuist";
+        }
+
         // If there's no error than..
         // Put session_start above
         if(!isset($error)) {
@@ -66,9 +68,6 @@ or die("Error: ". mysqli_connect_error());
     }
 
     // Am I loggin in? Please go to secure page
-
-
-
 
 ?>
 
@@ -101,9 +100,10 @@ or die("Error: ". mysqli_connect_error());
                       </div>
                       <div class="login__input">
                           <button class="login__submit" type="submit" name="submit">
-                              <h2> Login </h2>
+                              <h2 class="login__submit-title"> Login </h2>
                           </button>
                       </div>
+                      <p class="login__error"><?= $error ?></p>
                   </div>
               </div>
           </form>
